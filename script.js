d88986d5fb5d2f30ab0e54bd6240b3f21f71c89d
Open Source Coding <116367908+opensource-coding@users.noreply.github.com>
2022-11-11 10:24:36 +0500

    Final

--- script.js
+++ script.js
@@ -1,74 +1,473 @@
-const customPicker = document.querySelectorAll(".custom-picker");
-const colorPicker = document.querySelectorAll(".color-picker");
-
-customPicker.forEach((item) => {
-  item.addEventListener("click", () => {
-    item.querySelector(".color-picker").click();
+const _0x1992b0 = _0x5ebb;
+(function (_0x5e6111, _0x48fe0d) {
+  const _0x398d7a = _0x5ebb,
+    _0x3c7d26 = _0x5e6111();
+  while (!![]) {
+    try {
+      const _0x203c0e =
+        (-parseInt(_0x398d7a(0x190)) / 0x1) *
+          (-parseInt(_0x398d7a(0x1b9)) / 0x2) +
+        (parseInt(_0x398d7a(0x1a7)) / 0x3) *
+          (parseInt(_0x398d7a(0x1b8)) / 0x4) +
+        (parseInt(_0x398d7a(0x1d2)) / 0x5) *
+          (-parseInt(_0x398d7a(0x18c)) / 0x6) +
+        (parseInt(_0x398d7a(0x1d9)) / 0x7) *
+          (parseInt(_0x398d7a(0x192)) / 0x8) +
+        -parseInt(_0x398d7a(0x19b)) / 0x9 +
+        (-parseInt(_0x398d7a(0x1b1)) / 0xa) *
+          (-parseInt(_0x398d7a(0x1c9)) / 0xb) +
+        (-parseInt(_0x398d7a(0x175)) / 0xc) *
+          (-parseInt(_0x398d7a(0x1d0)) / 0xd);
+      if (_0x203c0e === _0x48fe0d) break;
+      else _0x3c7d26["push"](_0x3c7d26["shift"]());
+    } catch (_0x156caf) {
+      _0x3c7d26["push"](_0x3c7d26["shift"]());
+    }
+  }
+})(_0x4f6a, 0xa6436);
+function _0x5ebb(_0x528b01, _0x2373a5) {
+  const _0x4f6a55 = _0x4f6a();
+  return (
+    (_0x5ebb = function (_0x5ebbb1, _0x284368) {
+      _0x5ebbb1 = _0x5ebbb1 - 0x173;
+      let _0x174c4b = _0x4f6a55[_0x5ebbb1];
+      return _0x174c4b;
+    }),
+    _0x5ebb(_0x528b01, _0x2373a5)
+  );
+}
+const navitems = document[_0x1992b0(0x19c)](_0x1992b0(0x1dc)),
+  containers = document["querySelectorAll"](_0x1992b0(0x1af));
+function _0x4f6a() {
+  const _0x38b62e = [
+    "left",
+    "320gSKIjf",
+    "1112226zrroKi",
+    "querySelector",
+    "Please\x20select\x20an\x20image\x20file\x20(jpg\x20or\x20png)",
+    "target",
+    "((\x5cd{1,3}\x5c.){3}\x5cd{1,3}))",
+    "change",
+    "click",
+    "_blank",
+    ".dropzone",
+    ".upload-img",
+    "#file",
+    "value",
+    "display",
+    "file",
+    "image/jpg",
+    "download-svg",
+    "33LcpzNi",
+    "svg",
+    "corner-color",
+    "add",
+    "dots-style",
+    "input[name=\x22logo\x22]",
+    "src",
+    "879866ZwlpLt",
+    "style",
+    "5cUcqgu",
+    ".selected",
+    ".color-picker",
+    "bg-color",
+    "block",
+    "nextSibling",
+    "-container",
+    "7qfvHhm",
+    "px))",
+    "files",
+    "nav\x20div",
+    "open-source-coding-",
+    "active",
+    "dragover",
+    "span",
+    ".dropzone\x20.text",
+    "24gdbelo",
+    "catch",
+    "download-png",
+    "text",
+    "symbol",
+    "max",
+    "parentElement",
+    "json",
+    "now",
+    "data",
+    "result",
+    "calc(",
+    "download-jpg",
+    "Scanning\x20QR\x20Code...",
+    "png",
+    ".qr-code-img",
+    "addEventListener",
+    "jpg",
+    "readAsDataURL",
+    "indexOf",
+    "then",
+    "size",
+    "img",
+    "7453734SYojPk",
+    ".custom-slider\x20span",
+    "drop",
+    "download",
+    "1ybHvXV",
+    "https://www.youtube.com/channel/UCiUtBDVaSmMGKxg1HYeK-BQ",
+    "4255320fwBAeH",
+    "getElementById",
+    "input",
+    "append",
+    "http://api.qrserver.com/v1/read-qr-code/",
+    "DOMContentLoaded",
+    "anonymous",
+    "min",
+    "Upload\x20QR\x20Code\x20to\x20Scan",
+    "3995784DKFggZ",
+    "querySelectorAll",
+    "(\x5c?[;&a-z\x5cd%_.~+=-]*)?",
+    "remove",
+    "(\x5c#[-a-z\x5cd_]*)?$",
+    "^(https?:\x5c/\x5c/)?",
+    "length",
+    "textContent",
+    "input[type=text]",
+    "classList",
+    "writeText",
+    "innerText",
+    "24267xTBMxY",
+    "copy",
+    "Couldn\x27t\x20scan\x20QR\x20Code",
+    "forEach",
+    "show",
+    ".generate-btn",
+    "innerHTML",
+    ".custom-slider\x20input",
+    ".container",
+    "open",
+    "1655890dRgIxP",
+    "clipboard",
+    "preventDefault",
+    "#upload-img-input",
+    ".content",
+    "stopPropagation",
+  ];
+  _0x4f6a = function () {
+    return _0x38b62e;
+  };
+  return _0x4f6a();
+}
+navitems[_0x1992b0(0x1aa)]((_0x5c01ea) => {
+  const _0x263232 = _0x1992b0;
+  _0x5c01ea[_0x263232(0x185)]("click", () => {
+    const _0x532df8 = _0x263232;
+    navitems[_0x532df8(0x1aa)]((_0x926b28) => {
+      const _0x3625ab = _0x532df8;
+      _0x926b28["classList"]["remove"](_0x3625ab(0x1de));
+    }),
+      _0x5c01ea["classList"][_0x532df8(0x1cc)](_0x532df8(0x1de)),
+      containers[_0x532df8(0x1aa)]((_0x450e0f) => {
+        const _0x263f96 = _0x532df8;
+        _0x450e0f[_0x263f96(0x1a4)][_0x263f96(0x19e)](_0x263f96(0x1de));
+      }),
+      document[_0x532df8(0x1ba)]("#" + _0x5c01ea["id"] + _0x532df8(0x1d8))[
+        _0x532df8(0x1a4)
+      ][_0x532df8(0x1cc)]("active");
   });
 });
-colorPicker.forEach((item) => {
-  item.addEventListener("change", (e) => {
-    color = e.target.value;
-    span = item.parentElement.querySelector("span");
-    input = item.parentElement.querySelector("input[type=text]");
-    span.style.backgroundColor = color;
-    input.value = color;
+const customPicker = document[_0x1992b0(0x19c)](".custom-picker"),
+  colorPicker = document[_0x1992b0(0x19c)](_0x1992b0(0x1d4));
+customPicker[_0x1992b0(0x1aa)]((_0x33d2c4) => {
+  const _0x16ebf5 = _0x1992b0;
+  _0x33d2c4[_0x16ebf5(0x185)](_0x16ebf5(0x1bf), () => {
+    const _0x2f29a = _0x16ebf5;
+    _0x33d2c4[_0x2f29a(0x1ba)](_0x2f29a(0x1d4))[_0x2f29a(0x1bf)]();
   });
-});
-
-const customDropdown = document.querySelectorAll(".custom-dropdown");
-
-customDropdown.forEach((item) => {
-  //select all options inside a custom dropdown
-  options = item.querySelectorAll(".option");
-  // add event listner on all options
-  options.forEach((option) => {
-    option.addEventListener("click", () => {
-      //select all options of current options parent to remove active class
-      allOptions = option.parentElement.querySelectorAll(".option");
-      allOptions.forEach((item) => {
-        item.classList.remove("active");
-      });
-      //add active on clicked
-      option.classList.add("active");
-      //update selected text
-      item.querySelector(".selected").innerHTML = option.innerHTML;
+}),
+  colorPicker[_0x1992b0(0x1aa)]((_0x3d7270) => {
+    const _0xd6fb9a = _0x1992b0;
+    _0x3d7270[_0xd6fb9a(0x185)](_0xd6fb9a(0x1be), (_0x1be55d) => {
+      const _0x59c0c5 = _0xd6fb9a;
+      (color = _0x1be55d[_0x59c0c5(0x1bc)][_0x59c0c5(0x1c4)]),
+        (span = _0x3d7270[_0x59c0c5(0x17b)][_0x59c0c5(0x1ba)](
+          _0x59c0c5(0x173)
+        )),
+        (input = _0x3d7270[_0x59c0c5(0x17b)]["querySelector"](
+          _0x59c0c5(0x1a3)
+        )),
+        (span[_0x59c0c5(0x1d1)]["backgroundColor"] = color),
+        (input[_0x59c0c5(0x1c4)] = color);
     });
   });
-});
-
-const uploadElem = document.querySelector(".upload-img");
-const uploadImgInput = document.querySelector("#upload-img-input");
-
-uploadElem.addEventListener("click", () => {
-  uploadImgInput.click();
-});
-
-uploadImgInput.addEventListener("change", (e) => {
-  //on change get the file content
-  const file = e.target.files[0];
-  //reader the file with FileReader
-  const reader = new FileReader();
-  reader.readAsDataURL(file);
-  reader.onload = () => {
-    //img is next to file input select it and changr src
-    img = uploadImgInput.nextSibling.nextSibling;
-    img.src = reader.result;
+const uploadElem = document["querySelector"](_0x1992b0(0x1c2)),
+  uploadImgInput = document["querySelector"](_0x1992b0(0x1b4));
+uploadElem[_0x1992b0(0x185)](_0x1992b0(0x1bf), () => {
+  const _0x4f0b7d = _0x1992b0;
+  uploadImgInput[_0x4f0b7d(0x1bf)]();
+}),
+  uploadImgInput[_0x1992b0(0x185)](_0x1992b0(0x1be), (_0x9fd929) => {
+    const _0x5e0373 = _0x1992b0,
+      _0x4f4fcb = _0x9fd929[_0x5e0373(0x1bc)][_0x5e0373(0x1db)][0x0],
+      _0x356d5a = new FileReader();
+    _0x356d5a[_0x5e0373(0x187)](_0x4f4fcb),
+      (_0x356d5a["onload"] = () => {
+        const _0x1b84c1 = _0x5e0373;
+        (img = uploadImgInput["nextSibling"][_0x1b84c1(0x1d7)]),
+          (img[_0x1b84c1(0x1cf)] = _0x356d5a[_0x1b84c1(0x17f)]),
+          generateQRCode();
+      });
+  });
+const range = document[_0x1992b0(0x1ba)](_0x1992b0(0x1ae)),
+  tooltip = document[_0x1992b0(0x1ba)](_0x1992b0(0x18d)),
+  setValue = () => {
+    const _0x1dcb3a = _0x1992b0,
+      _0x17534b = Number(
+        ((range[_0x1dcb3a(0x1c4)] - range[_0x1dcb3a(0x199)]) * 0x64) /
+          (range[_0x1dcb3a(0x17a)] - range[_0x1dcb3a(0x199)])
+      ),
+      _0x2dbea4 = 0x10 - _0x17534b * 0.32;
+    (tooltip[_0x1dcb3a(0x1ad)] =
+      range[_0x1dcb3a(0x1c4)] + "\x20x\x20" + range["value"]),
+      (tooltip["style"][_0x1dcb3a(0x1b7)] =
+        _0x1dcb3a(0x180) +
+        _0x17534b +
+        "%\x20+\x20(" +
+        _0x2dbea4 +
+        _0x1dcb3a(0x1da));
   };
+document[_0x1992b0(0x185)](_0x1992b0(0x197), setValue),
+  range[_0x1992b0(0x185)]("input", setValue);
+const customDropdown = document[_0x1992b0(0x19c)](".custom-dropdown");
+customDropdown[_0x1992b0(0x1aa)]((_0x41b761) => {
+  const _0x17a72c = _0x1992b0;
+  (options = _0x41b761[_0x17a72c(0x19c)](".option")),
+    options["forEach"]((_0x1d44ed) => {
+      const _0x41dd10 = _0x17a72c;
+      _0x1d44ed[_0x41dd10(0x185)](_0x41dd10(0x1bf), () => {
+        const _0x3d729c = _0x41dd10;
+        (allOptions = _0x1d44ed[_0x3d729c(0x17b)][_0x3d729c(0x19c)](".option")),
+          allOptions["forEach"]((_0x28e7bd) => {
+            const _0x3b3b04 = _0x3d729c;
+            _0x28e7bd[_0x3b3b04(0x1a4)]["remove"]("active");
+          }),
+          _0x1d44ed[_0x3d729c(0x1a4)][_0x3d729c(0x1cc)](_0x3d729c(0x1de)),
+          (_0x41b761[_0x3d729c(0x1ba)](_0x3d729c(0x1d3))[_0x3d729c(0x1ad)] =
+            _0x1d44ed[_0x3d729c(0x1ad)]),
+          generateQRCode();
+      });
+    });
 });
-
-const range = document.querySelector(".custom-slider input");
-const tooltip = document.querySelector(".custom-slider span");
-
-//move tooltip with the slider
-function setValue() {
-  const newValue = Number(
-    ((range.value - range.min) * 100) / (range.max - range.min)
-  );
-  const newPosition = 16 - newValue * 0.32;
-  tooltip.innerHTML = range.value + " x " + range.value;
-  tooltip.style.left = `calc(${newValue}% + (${newPosition}px)`;
+const generateBtn = document["querySelector"](_0x1992b0(0x1ac)),
+  container = document[_0x1992b0(0x1ba)](_0x1992b0(0x184)),
+  width = document[_0x1992b0(0x193)]("size"),
+  height = document[_0x1992b0(0x193)](_0x1992b0(0x18a)),
+  data = document[_0x1992b0(0x193)](_0x1992b0(0x178)),
+  forgroundColor = document[_0x1992b0(0x193)]("fg-color"),
+  backgroundColor = document[_0x1992b0(0x193)](_0x1992b0(0x1d5)),
+  cornerColor = document[_0x1992b0(0x193)](_0x1992b0(0x1cb)),
+  imageRadios = document[_0x1992b0(0x19c)](_0x1992b0(0x1ce)),
+  dotsStyle = document["getElementById"](_0x1992b0(0x1cd)),
+  cornerSquaresStyle = document[_0x1992b0(0x193)]("corner-squares-style"),
+  cornerDotsStyle = document["getElementById"]("corner-dots-style");
+width[_0x1992b0(0x185)](_0x1992b0(0x1be), generateQRCode),
+  height[_0x1992b0(0x185)](_0x1992b0(0x1be), generateQRCode),
+  data[_0x1992b0(0x185)](_0x1992b0(0x194), generateQRCode),
+  forgroundColor["addEventListener"](_0x1992b0(0x1be), generateQRCode),
+  backgroundColor[_0x1992b0(0x185)]("change", generateQRCode),
+  cornerColor[_0x1992b0(0x185)]("change", generateQRCode),
+  generateBtn[_0x1992b0(0x185)]("click", generateQRCode),
+  imageRadios[_0x1992b0(0x1aa)]((_0xebed22) => {
+    const _0x4597a6 = _0x1992b0;
+    _0xebed22[_0x4597a6(0x185)]("change", generateQRCode);
+  });
+function generateQRCode() {
+  const _0x2fff5c = _0x1992b0;
+  let _0x5e5442 = document[_0x2fff5c(0x1ba)](
+      "input[name=\x22logo\x22]:checked"
+    ),
+    _0x3048a3 = document[_0x2fff5c(0x193)](_0x5e5442[_0x2fff5c(0x1c4)]);
+  (qrCode = new QRCodeStyling({
+    width: width[_0x2fff5c(0x1c4)],
+    height: height[_0x2fff5c(0x1c4)],
+    type: "canvas",
+    data:
+      data[_0x2fff5c(0x1c4)] !== "" ? data[_0x2fff5c(0x1c4)] : _0x2fff5c(0x191),
+    image: _0x3048a3[_0x2fff5c(0x1cf)],
+    imageOptions: {
+      saveAsBlob: !![],
+      crossOrigin: _0x2fff5c(0x198),
+      margin: 0xf,
+    },
+    dotsOptions: {
+      color: forgroundColor[_0x2fff5c(0x1c4)],
+      type: dotsStyle[_0x2fff5c(0x1ad)],
+    },
+    backgroundOptions: { color: backgroundColor[_0x2fff5c(0x1c4)] },
+    cornersSquareOptions: {
+      color: cornerColor["value"],
+      type: cornerSquaresStyle["innerHTML"],
+    },
+    cornersDotOptions: {
+      color: cornerColor[_0x2fff5c(0x1c4)],
+      type: cornerDotsStyle[_0x2fff5c(0x1ad)],
+    },
+  })),
+    (container[_0x2fff5c(0x1ad)] = ""),
+    qrCode["append"](container);
 }
-
-document.addEventListener("DOMContentLoaded", setValue);
-range.addEventListener("input", setValue);
+generateQRCode();
+const downloadPng = document[_0x1992b0(0x193)](_0x1992b0(0x177)),
+  downloadJpg = document[_0x1992b0(0x193)](_0x1992b0(0x181)),
+  downloadSvg = document["getElementById"](_0x1992b0(0x1c8));
+let name;
+downloadPng["addEventListener"](_0x1992b0(0x1bf), () => {
+  const _0x12f4a4 = _0x1992b0;
+  qrCode[_0x12f4a4(0x18f)]({
+    name: _0x12f4a4(0x1dd) + Date[_0x12f4a4(0x17d)](),
+    extension: _0x12f4a4(0x183),
+  });
+}),
+  downloadJpg[_0x1992b0(0x185)](_0x1992b0(0x1bf), () => {
+    const _0x420fe0 = _0x1992b0;
+    qrCode[_0x420fe0(0x18f)]({
+      name: _0x420fe0(0x1dd) + Date[_0x420fe0(0x17d)](),
+      extension: _0x420fe0(0x186),
+    });
+  }),
+  downloadSvg["addEventListener"](_0x1992b0(0x1bf), () => {
+    const _0x351c76 = _0x1992b0;
+    qrCode["download"]({
+      name: "open-source-coding-" + Date["now"](),
+      extension: _0x351c76(0x1ca),
+    });
+  });
+const dropzone = document[_0x1992b0(0x1ba)](_0x1992b0(0x1c1)),
+  dropzoneInput = document[_0x1992b0(0x1ba)](_0x1992b0(0x1c3)),
+  dropzoneText = document[_0x1992b0(0x1ba)](_0x1992b0(0x174));
+dropzoneInput["addEventListener"](_0x1992b0(0x1be), (_0x503962) => {
+  const _0x50154e = _0x1992b0;
+  if (dropzoneInput[_0x50154e(0x1db)][_0x50154e(0x1a1)]) {
+    let _0xc2e3ea = _0x503962[_0x50154e(0x1bc)][_0x50154e(0x1db)][0x0];
+    if (!_0xc2e3ea) return;
+    if (!checkFile(_0xc2e3ea)) return;
+    let _0x33eee5 = new FormData();
+    _0x33eee5[_0x50154e(0x195)](_0x50154e(0x1c6), _0xc2e3ea),
+      fetchRequest(_0xc2e3ea, _0x33eee5);
+  }
+});
+function updateThumbnail(_0x51fd8b) {
+  const _0x569990 = _0x1992b0;
+  let _0x1eb25c = new FileReader();
+  _0x1eb25c[_0x569990(0x187)](_0x51fd8b),
+    (_0x1eb25c["onload"] = () => {
+      const _0x5f5857 = _0x569990;
+      (content = dropzone["querySelector"](_0x5f5857(0x1b5))),
+        (_0x51fd8b = dropzone["querySelector"](_0x5f5857(0x18b))),
+        (_0x51fd8b[_0x5f5857(0x1cf)] = _0x1eb25c[_0x5f5857(0x17f)]),
+        _0x51fd8b[_0x5f5857(0x1a4)][_0x5f5857(0x1cc)](_0x5f5857(0x1ab)),
+        content[_0x5f5857(0x1a4)][_0x5f5857(0x19e)](_0x5f5857(0x1ab));
+    });
+}
+function reset() {
+  const _0x2e4333 = _0x1992b0;
+  (content = dropzone[_0x2e4333(0x1ba)](_0x2e4333(0x1b5))),
+    (img = dropzone[_0x2e4333(0x1ba)](_0x2e4333(0x18b))),
+    (img[_0x2e4333(0x1cf)] = ""),
+    img[_0x2e4333(0x1a4)][_0x2e4333(0x19e)]("show"),
+    content[_0x2e4333(0x1a4)]["add"]("show"),
+    (resultTextarea[_0x2e4333(0x1a6)] = "");
+}
+function checkFile(_0x15a0ed) {
+  const _0x40de21 = _0x1992b0,
+    _0x3dace2 = ["image/jpeg", _0x40de21(0x1c7), "image/png"];
+  if (_0x3dace2[_0x40de21(0x188)](_0x15a0ed["type"]) === -0x1)
+    return (
+      (dropzone[_0x40de21(0x1ba)](".text")[_0x40de21(0x1ad)] =
+        _0x40de21(0x1bb)),
+      ![]
+    );
+  return !![];
+}
+const handleDrop = (_0xbb3cd1) => {
+    const _0x4d6cca = _0x1992b0;
+    _0xbb3cd1[_0x4d6cca(0x1b3)](), _0xbb3cd1[_0x4d6cca(0x1b6)]();
+    const _0x20aa38 = _0xbb3cd1["dataTransfer"][_0x4d6cca(0x1db)][0x0];
+    dropzoneInput["files"] = _0xbb3cd1["dataTransfer"]["files"];
+    if (dropzoneInput["files"][_0x4d6cca(0x1a1)]) {
+      if (!_0x20aa38) return;
+      if (!checkFile(_0x20aa38)) return;
+      let _0x3e5736 = new FormData();
+      _0x3e5736[_0x4d6cca(0x195)]("file", _0x20aa38),
+        fetchRequest(_0x20aa38, _0x3e5736);
+    }
+  },
+  handleDragOver = (_0x4fcf7e) => {
+    const _0x52f7f1 = _0x1992b0;
+    _0x4fcf7e["preventDefault"](),
+      _0x4fcf7e[_0x52f7f1(0x1b6)](),
+      dropzone[_0x52f7f1(0x1a4)]["add"]("highlight");
+  },
+  handleDragLeave = (_0xcdc648) => {
+    const _0x207602 = _0x1992b0;
+    _0xcdc648[_0x207602(0x1b3)](),
+      _0xcdc648[_0x207602(0x1b6)](),
+      dropzone["classList"]["remove"]("highlight");
+  };
+dropzone[_0x1992b0(0x185)](_0x1992b0(0x1df), handleDragOver),
+  dropzone[_0x1992b0(0x185)]("dragleave", handleDragLeave),
+  dropzone["addEventListener"](_0x1992b0(0x18e), handleDrop);
+const resultTextarea = document[_0x1992b0(0x193)](_0x1992b0(0x17f)),
+  copyBtn = document[_0x1992b0(0x193)](_0x1992b0(0x1a8)),
+  openBtn = document[_0x1992b0(0x193)]("open");
+copyBtn["addEventListener"](_0x1992b0(0x1bf), () => {
+  const _0x3edbe6 = _0x1992b0;
+  (text = resultTextarea[_0x3edbe6(0x1a2)]),
+    navigator[_0x3edbe6(0x1b2)][_0x3edbe6(0x1a5)](text);
+}),
+  openBtn[_0x1992b0(0x185)](_0x1992b0(0x1bf), () => {
+    const _0x3f7369 = _0x1992b0;
+    (text = resultTextarea[_0x3f7369(0x1a2)]),
+      window[_0x3f7369(0x1b0)](text, _0x3f7369(0x1c0));
+  });
+function fetchRequest(_0x12c62f, _0x1a2e0e) {
+  const _0x2a98cf = _0x1992b0;
+  (dropzoneText[_0x2a98cf(0x1a6)] = _0x2a98cf(0x182)),
+    fetch(_0x2a98cf(0x196), { method: "POST", body: _0x1a2e0e })
+      [_0x2a98cf(0x189)]((_0xc44274) => _0xc44274[_0x2a98cf(0x17c)]())
+      ["then"]((_0x1104a4) => {
+        const _0x527d7f = _0x2a98cf;
+        (_0x1104a4 = _0x1104a4[0x0][_0x527d7f(0x179)][0x0][_0x527d7f(0x17e)]),
+          (dropzoneText[_0x527d7f(0x1a6)] = _0x1104a4
+            ? _0x527d7f(0x19a)
+            : _0x527d7f(0x1a9));
+        if (!_0x1104a4) {
+          reset();
+          return;
+        }
+        (resultTextarea[_0x527d7f(0x1a6)] = _0x1104a4),
+          document["querySelector"]("#result-btns")[_0x527d7f(0x1a4)][
+            _0x527d7f(0x1cc)
+          ](_0x527d7f(0x1de)),
+          !isValidUrl(_0x1104a4)
+            ? (openBtn[_0x527d7f(0x1d1)]["display"] = "none")
+            : (openBtn["style"][_0x527d7f(0x1c5)] = _0x527d7f(0x1d6)),
+          updateThumbnail(_0x12c62f);
+      })
+      [_0x2a98cf(0x176)](() => {
+        const _0x3e22d2 = _0x2a98cf;
+        reset(),
+          (dropzoneText[_0x3e22d2(0x1a6)] =
+            "Couldn\x27t\x20scan\x20QR\x20Code");
+      });
+}
+function isValidUrl(_0x3007ea) {
+  const _0x1f9da6 = _0x1992b0;
+  var _0x55cd83 = new RegExp(
+    _0x1f9da6(0x1a0) +
+      "((([a-z\x5cd]([a-z\x5cd-]*[a-z\x5cd])*)\x5c.)+[a-z]{2,}|" +
+      _0x1f9da6(0x1bd) +
+      "(\x5c:\x5cd+)?(\x5c/[-a-z\x5cd%_.~+]*)*" +
+      _0x1f9da6(0x19d) +
+      _0x1f9da6(0x19f),
+    "i"
+  );
+  return !!_0x55cd83["test"](_0x3007ea);
+}
